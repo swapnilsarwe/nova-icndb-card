ddbf0e13c18861dca5b2dac30d2143c3962c0e57
Swapnil Sarwe <swapnilsarwe@gmail.com>
2018-09-11 00:14:14 +0530

    change the name of the repo to more appropriate one

---
+++ resources/js/components/Card.vue
@@ -0,0 +1,36 @@
+<template>
+    <card class="flex flex-col items-center justify-center">
+        <div class="px-3 py-3">
+            <h2 class="text-center text-2xl text-80 font-light">
+                Chuck Norris Joke
+            </h2>
+            <div class="mt-4">
+                <span class="text-70">{{ joke }}</span>
+            </div>
+        </div>
+    </card>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            joke: '',
+        }
+    },
+
+    props: ['card'],
+
+    mounted() {
+        this.getJoke();
+    },
+
+    methods: {
+        getJoke() {
+            Nova.request().get('https://api.icndb.com/jokes/random').then(response => {
+                this.joke = response.data.value.joke
+            })
+        }
+    }
+}
+</script>
