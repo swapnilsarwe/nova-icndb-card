63d2c383094e9fd0af1ed4ce78b496c2419294b9
Swapnil Sarwe <swapnilsarwe@gmail.com>
2018-11-01 15:40:11 +0530

    added name configuration for icndb jokes

--- src/Http/Controllers/ICNDbJokeController.php
+++ src/Http/Controllers/ICNDbJokeController.php
@@ -7,16 +7,22 @@ use ICNDb;
 class ICNDbJokeController
 {
     private $icndbClient;
+    private $config;
 
     public function __construct()
     {
-        $config = [];
+        $this->config = config('icndb-card');
+        $nameToUse = $this->config['name_to_use'];
+        $config = [
+            'firstName' => array_get($nameToUse, 'first_name', 'Chuck'),
+            'lastName' => array_get($nameToUse, 'last_name', 'Norris'),
+        ];
         $this->icndbClient = new ICNDb\Client($config);
     }
     
     public function __invoke()
     {
-        $response = $this->icndbClient->random()->exclude(config('icndb-card')['excluded_categories'])->get();
+        $response = $this->icndbClient->random()->exclude($this->config['excluded_categories'])->get();
         return (count($response) > 0) ? $response[0]->joke : '';
     }
 }
--- src/config/icndb-card.php
+++ src/config/icndb-card.php
@@ -1,6 +1,10 @@
 <?php
 
 return [
+    'name_to_use' => [
+        'first_name' => 'Chuck',
+        'last_name' => 'Norris',
+    ],
     'excluded_categories' => [
         'explicit',
     ],
