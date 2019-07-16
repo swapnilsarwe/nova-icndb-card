7230912a254ae534876609fe1e893db2ec81bbf8
Swapnil Sarwe <swapnilsarwe@gmail.com>
2019-07-16 12:17:54 +0000

    Apply fixes from StyleCI

--- src/config/icndb-card.php
+++ src/config/icndb-card.php
@@ -3,7 +3,7 @@
 return [
     'name_to_use' => [
         'first_name' => 'Chuck', // type in your own first name
-        'last_name' => 'Norris', // type in your own last name
+        'last_name'  => 'Norris', // type in your own last name
     ],
     'excluded_categories' => [ // jokes belonging to following categories will not be shown
         'explicit',
